ef515744f8a9681a80bd21faf6830077ce95a23b
Niusha Yaghini <niusha.yaghini@gmail.com>
2026-09-13 10:46:37 +0330

    Selection + Subtree Crossover

--- 04-optimization-and-search/03-evolutionary-computation/src/GP.py
+++ 04-optimization-and-search/03-evolutionary-computation/src/GP.py
@@ -1,87 +1,6 @@
 import random
 import numpy as np
-
-
-FUNCTIONS = [
-    "+",
-    "-",
-    "*",
-    "/",
-]
-
-TERMINALS = [
-    "x",
-    1,
-    2,
-    3,
-]
-
-def fitness_function(
-    tree,
-    x_data,
-    y_data,
-):
-    predictions = []
-
-    for x in x_data:
-        try:
-            value = tree.evaluate(
-                x
-            )
-        except:
-            value = 1e6
-        predictions.append(
-            value
-        )
-
-    predictions = np.array(
-        predictions
-    )
-
-    error = np.mean(
-        (predictions - y_data)**2
-    )
-
-    return error
-
-
-def generate_random_tree(
-    max_depth,
-    current_depth=0,
-):
-    # Stop condition: If maximum depth reached, create terminal node
-    if current_depth >= max_depth:
-        return Node(
-            random.choice(
-                TERMINALS
-            )
-        )
-
-    # Decide node type
-    if random.random() < 0.7:
-        # Function Node
-        function = random.choice(
-            FUNCTIONS
-        )
-        return Node(
-            function,
-            generate_random_tree(
-                max_depth,
-                current_depth + 1,
-            ),
-            generate_random_tree(
-                max_depth,
-                current_depth + 1,
-            ),
-        )
-    else:
-        # Terminal Node
-        return Node(
-            random.choice(
-                TERMINALS
-            )
-        )
-        
+import copy
 
 class Node:
     def __init__(
@@ -173,3 +92,161 @@ class Node:
             +
             ")"
         )
+    
+    def get_nodes(
+        self,
+    ):
+        nodes = [self]
+        if self.left is not None:
+            nodes.extend(
+                self.left.get_nodes()
+            )
+        if self.right is not None:
+            nodes.extend(
+                self.right.get_nodes()
+            )
+        return nodes
+    
+    def copy_tree(
+        self,
+    ):
+        return copy.deepcopy(
+            self
+        )
+        
+        
+FUNCTIONS = [
+    "+",
+    "-",
+    "*",
+    "/",
+]
+
+TERMINALS = [
+    "x",
+    1,
+    2,
+    3,
+]
+
+def generate_random_tree(
+    max_depth,
+    current_depth=0,
+):
+    # Stop condition: If maximum depth reached, create terminal node
+    if current_depth >= max_depth:
+        return Node(
+            random.choice(
+                TERMINALS
+            )
+        )
+
+    # Decide node type
+    if random.random() < 0.7:
+        # Function Node
+        function = random.choice(
+            FUNCTIONS
+        )
+        return Node(
+            function,
+            generate_random_tree(
+                max_depth,
+                current_depth + 1,
+            ),
+            generate_random_tree(
+                max_depth,
+                current_depth + 1,
+            ),
+        )
+    else:
+        # Terminal Node
+        return Node(
+            random.choice(
+                TERMINALS
+            )
+        )
+
+
+def fitness_function(
+    tree,
+    x_data,
+    y_data,
+):
+    predictions = []
+
+    for x in x_data:
+        try:
+            value = tree.evaluate(
+                x
+            )
+        except:
+            value = 1e6
+        predictions.append(
+            value
+        )
+
+    predictions = np.array(
+        predictions
+    )
+
+    error = np.mean(
+        (predictions - y_data)**2
+    )
+
+    return error
+
+
+def tournament_selection(
+    population,
+    fitness_values,
+    tournament_size=3,
+):
+    # Step 1: Randomly choose individuals
+    participants = random.sample(
+        range(len(population)),
+        tournament_size,
+    )
+
+    # Step 2: Find best individual (Lower fitness is better)
+    best_index = participants[0]
+
+    for index in participants[1:]:
+        if fitness_values[index] < fitness_values[best_index]:
+            best_index = index
+
+    # Step 3: Return selected Tree
+    return population[best_index]
+
+
+def get_random_node(
+    tree,
+):
+    nodes = tree.get_nodes()
+    return random.choice(
+        nodes
+    )
+    
+
+def subtree_crossover(
+    parent1,
+    parent2,
+):
+    # Copy parents
+    child = parent1.copy_tree()
+
+    # Select random subtree from parent2
+    donor_subtree = get_random_node(
+        parent2
+    )
+
+    # Select random node in child
+    target_node = get_random_node(
+        child
+    )
+
+    # Replace target value
+    target_node.value = donor_subtree.value
+    target_node.left = donor_subtree.left
+    target_node.right = donor_subtree.right
+
+    return child
