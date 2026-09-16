5c4cb3e14233b95e427b39d85b9683783887768c
Niusha Yaghini <niusha.yaghini@gmail.com>
2026-09-16 19:21:46 +0330

    model evaluation

---
+++ 05-machine-learning/model_evaluation/README.md
@@ -0,0 +1,19 @@
+Regression:
+MAE
+MSE
+RMSE
+R²
+Residual Analysis
+
+Classification:
+Accuracy
+Precision
+Recall
+F1
+ROC-AUC
+Confusion Matrix
+
+Model Selection:
+Cross Validation
+Bias-Variance Tradeoff
+Learning Curves
---
+++ 05-machine-learning/model_evaluation/src/regression_metrics.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def mean_absolute_error(
+    y_true,
+    y_pred
+):
+
+    return np.mean(
+        np.abs(
+            y_true - y_pred
+        )
+    )
+    
+def mean_squared_error(
+    y_true,
+    y_pred
+):
+
+    return np.mean(
+        (
+            y_true-y_pred
+        )**2
+    )
+    
+def root_mean_squared_error(
+    y_true,
+    y_pred
+):
+
+    mse = mean_squared_error(
+        y_true,
+        y_pred
+    )
+
+    return np.sqrt(mse)
+
+def r2_score(
+    y_true,
+    y_pred
+):
+
+    ss_res = np.sum(
+        (y_true-y_pred)**2
+    )
+
+    ss_tot = np.sum(
+        (y_true-np.mean(y_true))**2
+    )
+
+    return 1 - (
+        ss_res / ss_tot
+    )
